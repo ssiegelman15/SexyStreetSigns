8c4cc08c92f87d49a9d6ed2a4c4036f4c81842df
Marcus mbp <maherreraiv1987@gmail.com>
2022-09-14 20:56:28 -0600

    vehicle schema started

---
+++ server/models/Vehicle.js
@@ -0,0 +1,29 @@
+const { Schema } = require('mongoose');
+
+const vehicleSchema = new Schema({
+	name: {
+		type: String,
+		unique: true,
+		required: true
+	},
+	make: {
+		type: String,
+		required: true
+	},
+	model: {
+		type: String,
+		required: true
+	},
+	mpg: {
+		type: Number
+	},
+	color: {
+		type: String
+	},
+	carType: {
+		type: String,
+		required: true
+	}
+});
+
+module.exports = vehicleSchema;
