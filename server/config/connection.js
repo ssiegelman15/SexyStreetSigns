4e0d110f1f2c2114cc242e5b0444de79292627e6
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-09-14 19:52:17 -0600

    changed db name to hippie-db

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/moovy-db',
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/hippie-db",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
